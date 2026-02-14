fcf01feb35eda16982f4f1b1e1f8df441cf411ba
A Certain Existence <a.certain.existence@gmail.com>
2026-02-14 15:46:43 +0800

    Add value per full stack sorter

---
+++ Source/TradeSortValuePerFullStack.cs
@@ -0,0 +1,13 @@
+﻿using RimWorld;
+
+namespace TD_Enhancement_Pack
+{
+	public class TransferableComparer_ValuePerFullStack : TransferableComparer
+	{
+		public override int Compare(Transferable lhs, Transferable rhs)
+		{
+			return (lhs.AnyThing.MarketValue * lhs.ThingDef.stackLimit)
+				.CompareTo(rhs.AnyThing.MarketValue * rhs.ThingDef.stackLimit);
+		}
+	}
+}
